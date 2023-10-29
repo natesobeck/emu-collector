c62abcd6287af029663898d867891a7fe8e13987
Nate Sobeck <nsobeck910@gmail.com>
2023-10-29 12:44:27 -0400

    add ai images for emus

--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse('<h1>Give me the emus!</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
