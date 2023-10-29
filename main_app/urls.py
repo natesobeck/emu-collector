a83894aa0fd6b5a0c1cd1582631c5b9501c0b3b5
Nate Sobeck <nsobeck910@gmail.com>
2023-10-29 13:44:39 -0400

    create template for emu index, add styling

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-  path('about/', views.about, name='about')
+  path('about/', views.about, name='about'),
+  path('emus/', views.emu_index, name='emu-index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+class Emu:
+  def __init__(self, name, age, description, attitude):
+    self.name = name
+    self.age = age
+    self.description = description
+    self.attitude = attitude
+
+emus = [
+  Emu('Fred', 15, 'Total bro', 'Chill'),
+  Emu('Barry', 4, 'Loves to mess with you', 'Mischievious'),
+  Emu('Sally', 6, 'She is sweet when she opens up', 'Shy')
+]
+
 
 def home(request):
   return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+def emu_index(request):
+  return render(request, 'emus/index.html', { 'emus': emus })
