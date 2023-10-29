62eeed4953e0911cf99b3b68acda79102754f6e2
Nate Sobeck <nsobeck910@gmail.com>
2023-10-29 14:17:09 -0400

    adjust emu index page to use database instead of hard coded data

--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,7 @@
 from django.shortcuts import render
+from .models import Emu
 
 # Create your views here.
-class Emu:
-  def __init__(self, name, age, description, attitude):
-    self.name = name
-    self.age = age
-    self.description = description
-    self.attitude = attitude
-
-emus = [
-  Emu('Fred', 15, 'Total bro', 'Chill'),
-  Emu('Barry', 4, 'Loves to mess with you', 'Mischievious'),
-  Emu('Sally', 6, 'She is sweet when she opens up', 'Shy')
-]
-
-
 def home(request):
   return render(request, 'home.html')
 
@@ -22,4 +9,5 @@ def about(request):
   return render(request, 'about.html')
 
 def emu_index(request):
+  emus = Emu.objects.all()
   return render(request, 'emus/index.html', { 'emus': emus })
