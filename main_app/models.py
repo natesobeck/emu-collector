88063120dbd63b3f9011047ff8219797c557e057
Nate Sobeck <nsobeck910@gmail.com>
2023-10-31 10:03:49 -0400

    add Feedings model and make migrations

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+)
+
 class Emu(models.Model):
   name = models.CharField(max_length=100)
   age = models.IntegerField()
@@ -12,3 +18,17 @@ class Emu(models.Model):
 
   def get_absolute_url(self):
     return reverse('emu-detail', kwargs={'emu_id': self.id})
+  
+class Feeding(models.Model):
+  date = models.DateField()
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+
+  emu = models.ForeignKey(Emu, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+
