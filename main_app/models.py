f90de963f241419c6e274738dcd3502369dff0f7
Nate Sobeck <nsobeck910@gmail.com>
2023-10-31 17:11:16 -0400

    implement conditional logic to give the user a message displaying if the emu has been fed all of their meals for the day

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 MEALS = (
   ('B', 'Breakfast'),
@@ -18,6 +19,9 @@ class Emu(models.Model):
 
   def get_absolute_url(self):
     return reverse('emu-detail', kwargs={'emu_id': self.id})
+  
+  def fed_for_today(self):
+    return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
   
 class Feeding(models.Model):
   date = models.DateField('Feeding Date')
